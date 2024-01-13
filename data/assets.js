6fee9123b3cfd86d56cbe999656d89aa910db019
thuy <tath0001@e.ntu.edu.sg>
2024-01-13 18:04:57 +0800

    Add BalanceSheet

---
+++ data/assets.js
@@ -0,0 +1,9 @@
+export const assetsData = [
+     //need more like this
+     {
+        id: 1,
+        assetType: '',
+        total: 2795.95,
+        status: '',
+    }
+];
---
+++ data/liabilities.js
@@ -0,0 +1,9 @@
+export const liabilitiesData = [
+   //need more like this
+    {
+        id: 1,
+        liabilityType: '',
+        total: 2795.95,
+        status: '',
+    }
+];
